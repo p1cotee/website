cb152d6711b99acf10d8ac9980bd359f4a982e80
p1cotee <izzy.wang.nw@gmail.com>
2025-12-12 19:55:50 -0800

    nav bar icons
    made nav bar icons

---
+++ src/navigation.jsx
@@ -0,0 +1,17 @@
+function Navigation(){
+    return(
+        <div>
+            <div className='bg-rect'></div>
+            <div className='nav-bar-about-me'></div>
+            <div className='nav-bar-projects'></div>
+            <div className='nav-bar-work'></div>
+            <div className='nav-bar-links'></div>
+            <div className='about-me-icon'></div>
+            <div className='projects-icon'></div>
+            <div className='work-icon'></div>
+            <div className='links-icon'></div>
+        </div>
+    );
+}
+
+export default Navigation;
